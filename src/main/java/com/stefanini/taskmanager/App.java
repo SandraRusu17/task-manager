517e484f6595b34effa9d3febf233b224993e0be
SandraRusu17 <sandra.rusu17@gmail.com>
2021-07-26 20:57:19 +0300

    -fix a major bug related to spaces in command line -refactor some code

--- src/main/java/com/stefanini/taskmanager/App.java
+++ src/main/java/com/stefanini/taskmanager/App.java
@@ -2,10 +2,8 @@ package com.stefanini.taskmanager;
 
 import com.stefanini.taskmanager.engine.TaskManager;
 
-import java.util.Arrays;
-
 public class App {
     public static void main(String[] args) {
-        new TaskManager().parseCommandLine(args);
+        new TaskManager().parseCommandArguments(args);
     }
 }
--- src/main/java/com/stefanini/taskmanager/engine/TaskManager.java
+++ src/main/java/com/stefanini/taskmanager/engine/TaskManager.java
@@ -6,6 +6,7 @@ import com.stefanini.taskmanager.repo.UserFileRepositoryImpl;
 import com.stefanini.taskmanager.service.UserService;
 import com.stefanini.taskmanager.service.UserServiceImpl;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
@@ -23,65 +24,71 @@ public class TaskManager {
     //    -deleteTask -un='UserName' -tt='TaskTitle'
     public static final String DELETE_TASK_COMMAND = "-deleteTask";
 
+    public static final int COMMAND = 0;
+
     private final UserFileRepositoryImpl userFileRepository = new UserFileRepositoryImpl();
     private final UserService userService = new UserServiceImpl(userFileRepository);
 
-    public void parseCommandLine(String[] commandLine) {
-        if(commandLine.length < 1){
-            System.out.println("Oops. Please use one of the following commands: -createUser -showAllUsers -addTask -showTasks");
+    public void parseCommandArguments(String[] arguments) {
+        String joinedArguments = String.join(" ", arguments); //so we can have arguments with spaces
+        String[] commandAndParameters = joinedArguments.split(" -"); //split by ' -' again so we have the command and its parameters
+
+        if (commandAndParameters.length < 1) {
+            System.out.println("Oops. Please use one of the following commands: -createUser -showAllUsers -addTask -showTasks -deleteTask");
             System.exit(0);
         }
-        final String command = commandLine[0];
+        final String command = commandAndParameters[COMMAND];
         switch (command) {
             case CREATE_USER_COMMAND:
-                if(commandLine.length < 4){
+                if (commandAndParameters.length < 4) {
                     System.out.println("Oops. Please refer to the usage of the command : -createUser -fn='FirstName' -ln='LastName' -un='UserName'");
                     break;
                 }
-                userService.createUser(
-                        getStringValue(commandLine[3]),
-                        getStringValue(commandLine[2]),
-                        getStringValue(commandLine[1]));
+                String firstName = getStringValue(commandAndParameters[1]);
+                String lastName = getStringValue(commandAndParameters[2]);
+                String username = getStringValue(commandAndParameters[3]);
+                userService.createUser(username, firstName, lastName);
                 break;
             case SHOW_ALL_USERS_COMMAND:
                 System.out.println(formatUsers(userService.getAllUsers()));
                 break;
             case ADD_TASK_COMMAND:
-                if(commandLine.length < 4){
+                if (commandAndParameters.length < 4) {
                     System.out.println("Oops. Please refer to the usage of the command : -addTask -un='UserName' -tt='TaskTitle' -td='TaskDescription'");
                     break;
                 }
+                String taskToAddUsername = getStringValue(commandAndParameters[1]);
+                String taskToAddTitle = getStringValue(commandAndParameters[2]);
+                String taskToAddDescription = getStringValue(commandAndParameters[3]);
                 try {
-                    userService.addTaskFor(
-                            getStringValue(commandLine[2]),
-                            getStringValue(commandLine[3]),
-                            getStringValue(commandLine[1]));
+                    userService.addTaskFor(taskToAddTitle, taskToAddDescription, taskToAddUsername);
                 } catch (UserNotFoundException e) {
-                    System.out.println("Oops. User with username " + commandLine[1] + " not found!");
+                    System.out.println("Oops. User with username " + taskToAddUsername + " not found!");
                 }
                 break;
             case DELETE_TASK_COMMAND:
+                String taskToDeleteTitle = getStringValue(commandAndParameters[2]);
+                String taskToDeleteUsername = getStringValue(commandAndParameters[1]);
                 try {
-                    userService.deleteTaskByTitleFor(
-                            getStringValue(commandLine[2]),
-                            getStringValue(commandLine[1]));
+                    userService.deleteTaskByTitleFor(taskToDeleteTitle, taskToDeleteUsername);
                 } catch (UserNotFoundException e) {
-                    System.out.println("Oops. User with username " + commandLine[1] + " not found!");
+                    System.out.println("Oops. User with username " + taskToDeleteUsername + " not found!");
                 }
                 break;
             case SHOW_TASKS_COMMAND:
-                if(commandLine.length < 2){
+                if (commandAndParameters.length < 2) {
                     System.out.println("Oops. Please refer to the usage of the command : -showTasks -un='UserName'");
                     break;
                 }
+                String showTasksUsername = getStringValue(commandAndParameters[1]);
                 try {
-                    System.out.println(userService.getTasksFor(getStringValue(commandLine[1])));
+                    System.out.println(userService.getTasksFor(showTasksUsername));
                 } catch (UserNotFoundException e) {
-                    System.out.println("Oops. User with username " + commandLine[1] + " not found!");
+                    System.out.println("Oops. User with username " + showTasksUsername + " not found!");
                 }
                 break;
             default:
-                System.out.println("Unknown command");
+                System.out.println("Unknown command. Please use one of the following commands: -createUser -showAllUsers -addTask -showTasks -deleteTask");
         }
     }
 
